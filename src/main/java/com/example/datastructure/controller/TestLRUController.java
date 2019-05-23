c9aa20401da3a3d31a33196d4b9d3eca2a4f5086
mylzj <864716024@qq.com>
2019-05-23 10:42:18 +0800

    测试LRU

---
+++ src/main/java/com/example/datastructure/controller/TestLRUController.java
@@ -0,0 +1,25 @@
+package com.example.datastructure.controller;
+
+import com.example.datastructure.LinkedList.LRUByLinkedHashMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Demo class
+ *
+ * @author drose
+ * @date 2019/5/23 10:31
+ */
+@Controller
+@RequestMapping("/LRU")
+public class TestLRUController {
+    private LRUByLinkedHashMap lru=new LRUByLinkedHashMap(5);
+
+    @ResponseBody
+    @GetMapping("/add/{value}")
+    public String getAll(@PathVariable String value){
+        this.lru.add(value);
+        this.lru.getAll();
+        return "200";
+    }
+}
