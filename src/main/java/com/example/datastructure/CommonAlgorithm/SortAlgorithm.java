ac00ab044ca07f685560b3c8ded9a1069fa0b39c
mylzj <864716024@qq.com>
2021-01-21 23:59:58 +0800

    feat:快速排序第一版

--- src/main/java/com/example/datastructure/CommonAlgorithm/SortAlgorithm.java
+++ src/main/java/com/example/datastructure/CommonAlgorithm/SortAlgorithm.java
@@ -10,10 +10,12 @@ import java.util.List;
  */
 public class SortAlgorithm {
     public static void main(String[] args) {
-        int[] a = new int[]{5, 4, 3, 7, 1};
+        int[] a = new int[]{9,7,11,6,8};
 //        insertSort(a);
 //        selectSort(a);
-        bubbleSort(a);
+//        bubbleSort(a);
+
+        quickSort(a);
         for (int i : a) {
             System.out.print(i);
         }
@@ -58,7 +60,7 @@ public class SortAlgorithm {
 
         for (int i = 0; i < n; i++) {
             boolean flag = false;
-            for (int j = 0; j < n-1-i; j++) {
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (a[j] > a[j + 1]) {
                     int temp = a[j];
                     a[j] = a[j + 1];
@@ -66,9 +68,58 @@ public class SortAlgorithm {
                     flag = true;
                 }
             }
-            if(!flag){
+            if (!flag) {
                 break;
             }
         }
+    }
+
+    public static void quickSort(int[] a) {
+        // 快排也是递归的一种体现，快排的思想就是，在集合里面随机取一个数，然后与这个数作比较，比这个数小的放左边，
+        // 比这个数大的放右边。然后以这个数为节点，重新排序左边和右边。以此递归，最后合并成一个。
+        // 确定递归思想，推出递归公式，sort(Array(n)) = sort(Array(0,i-1))+sort(Array(i,n))
+
+        int n = a.length;
+        merge(a, 0, n-1);
+
+    }
+
+    private static void merge(int[] a, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int part = part(a, start, end-1);
+        merge(a, start, part - 1);
+        merge(a, part + 1, end);
+
+    }
+
+    /**
+     * 对数组进行排序，用一个下标i来区分，将数据分成两批，
+     * 一批排序的（初始都是指向第一个数），一批未排序。最后返回i的位置，即是那个随机数的下标
+     *
+     * @param a
+     * @param start
+     * @param end
+     * @return
+     */
+    private static int part(int[] a, int start, int end) {
+        int pivot = a[end];
+        int j = start;
+        for (int i = start; i < end; i++) {
+            if (a[i] < pivot) {
+                swap(a, i, j);
+                j++;
+            }
+        }
+        swap(a, j, end);
+        return j;
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
